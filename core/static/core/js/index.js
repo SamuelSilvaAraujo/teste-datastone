c0ec7217b010d03dc81b096528f9a6aa11ac7416
Samuel Araújo Silva <araujo.samuel.silva@gmail.com>
2023-11-14 23:04:46 -0300

    adicionando função ajax

--- core/static/core/js/index.js
+++ core/static/core/js/index.js
@@ -1,1 +1,20 @@
-const submitForm = () => {};
+document.getElementById("converter_form").addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  $.ajax({
+    url: converterApiUrl,
+    method: "post",
+    headers: { "X-CSRFToken": csrftoken },
+    data: {
+      value: document.getElementById("id_value").value,
+      from_coin: document.getElementById("id_from").value,
+      to_coin: document.getElementById("id_to").value,
+    },
+    success: function (response) {
+      document.getElementById("id_result").value = response["result"];
+    },
+    error: function (error) {
+      console.log(error);
+    },
+  });
+});
